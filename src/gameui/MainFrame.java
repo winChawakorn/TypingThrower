eb16a716bb17f19b7b1e6603cb3b245814d235cd
Chawakorn Suphepre <win.chawakorn@gmail.com>
2017-05-25 21:51:39 +0700

    remove some non use comment, add count down before game start, and modified online checker system

--- src/gameui/MainFrame.java
+++ src/gameui/MainFrame.java
@@ -92,7 +92,7 @@ public class MainFrame {
 	public MainFrame() {
 		Controller ctrl = Controller.getInstance();
 		ctrl.setClient(new Client("35.185.188.93", 3007));
-		initialize();
+		initialize();	
 	}
 
 	/**
