66df674235e84403cc6f2ae7c1688fac35520db0
Vittunyuta Maeprasart <vittunyuta@gmail.com>
2017-05-25 20:15:46 +0700

    check

--- src/gameui/MainFrame.java
+++ src/gameui/MainFrame.java
@@ -79,7 +79,7 @@ public class MainFrame {
 	 */
 	public MainFrame() {
 		Controller ctrl = Controller.getInstance();
-		ctrl.setClient(new Client("35.185.188.93", 3007));
+		ctrl.setClient(new Client("", 3007));
 		initialize();
 	}
 
